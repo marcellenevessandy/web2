578238a93a999f418fcdc06091513a0ce1b2c2b8
Marcelle Neves Sandy <146496555+marcellenevessandy@users.noreply.github.com>
2024-11-04 22:24:08 -0300

    Primeiro Commit

---
+++ Projeto1/exers/exer1.php
@@ -0,0 +1,34 @@
+<?php
+// Todo código php precisa estar dentro de <?php ? >
+
+//Envia um expressão ou mais para saída
+echo "Hello World" . "<br>";
+
+//Como criar uma variavel em php
+$nome = "Marcelle";
+$sobrenome = "Neves Sandy";
+$idade = 21;
+$alura = 1.77;
+
+// Concatenar php usar sinal . (ponto)
+echo "$nome " . "$sobrenome" . "<br>";
+echo "Idade:" . $idade;
+echo "<h1 style='color:blue; text-align:center;'> " . $nome . "<br>" . $sobrenome . "</h1>";
+
+//Sem concatenar
+echo "<h1 style='color:red; text-align:center;'> $nome <br> $sobrenome </h1>";
+
+// Você pode colocar estilo dentro do php
+echo "<h1 style='color:pink; text-align:center;'> " . $nome . "</h1>";
+echo "<h1 style='color:green; text-align:row;'> " . $nome . "<br>" . $sobrenome . "</h1>";
+
+//Calcular IMC
+$nome = "Marcelle";
+$peso = 95;
+$altura = 1.70;
+$imc = $peso / pow($altura, 2);
+
+//Para usar o number_format precisa concatenar com ponto 
+echo "Nome: $nome <br>" . "Peso:" . number_format($peso, 2)
+. "<br> Altura:" . number_format($altura, 2)
+. " <br> IMC:" . number_format($imc, 2);
---
+++ Projeto1/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu Principal</title>
+</head>
+<body>
+  
+<header>
+
+<h3>Projetos</h3> 
+<h3>Exercícios</h3>
+
+</header>
+
+<main>
+    <div class = "container">
+        <a href="./exer1.php">Exercício 1</a>
+        <a href="#">Exercício 2</a>
+        <a href="#">Exercício 3</a>
+    </div>
+
+</main>
+
+<footer>
+    
+</footer>
+
+</body>
+</html>
